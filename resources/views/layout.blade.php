b9e8962ff965cfecbf3c4c6bea393aa66c134322
Patrick Neary <patrick@red-fern.co.uk>
2019-09-23 16:17:13 +0100

    Created base layout file

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Laracasts')</title>
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
 </head>
 <body>
     <ul>
