cf912aa2b4c346ad813ed8b4b329fa8e06bf16a6
Patrick Neary <patrick@red-fern.co.uk>
2019-09-25 16:50:19 +0100

    Added bootstrap container and row to main views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,11 +7,17 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
 </head>
 <body>
-    <ul>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </ul>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <ul>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </ul>
+        </div>
+    </div>
+</div>
 
 @yield('Content')
 </body>
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -2,44 +2,51 @@
 
 @section('Content')
 
-<h2>{{  $project->title  }}</h2>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>{{  $project->title  }}</h2>
+            <form action="/projects/{{ $project->id }}" method="POST">
 
-<form action="/projects/{{ $project->id }}" method="POST">
+                {{ method_field('PATCH') }}
 
-    {{ method_field('PATCH') }}
+                @csrf
 
-    @csrf
+                <div class="field">
 
-    <div class="field">
+                    <label for="title" class="label">Title</label>
 
-        <label for="title" class="label">Title</label>
+                    <div class="control">
+                        <input type="text" class="input" name="title" placeholder="Title">
+                    </div>
 
-        <div class="control">
-            <input type="text" class="input" name="title" placeholder="Title">
+                </div>
+
+                <div class="field">
+
+                    <label for="description" class="label">Description</label>
+
+                    <div class="control">
+                        <textarea name="description" class="textarea"></textarea>
+                    </div>
+
+                </div>
+
+                <div class="field">
+
+                    <div class="control">
+
+                        <button type="submit" class="is-link button">Update Project</button>
+
+                    </div>
+
+                </div>
+
+            </form>
+
         </div>
-
     </div>
+</div>
 
-    <div class="field">
-
-        <label for="description" class="label">Description</label>
-
-        <div class="control">
-            <textarea name="description" class="textarea"></textarea>
-        </div>
-
-    </div>
-
-    <div class="field">
-
-        <div class="control">
-
-            <button type="submit" class="is-link button">Update Project</button>
-
-        </div>
-
-    </div>
-
-</form>
 
 @endsection
