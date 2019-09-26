e8b33126d1988ee00d3f4d3a1b34c0d381d9b54d
Patrick Neary <patrick@red-fern.co.uk>
2019-09-26 16:27:48 +0100

    Added initial markup for the show view

---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,52 @@
+@extends('layout')
+
+@section('Content')
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>{{  $project->title  }} Show Page</h2>
+            <form action="/projects/{{ $project->id }}" method="POST">
+
+                {{ method_field('PATCH') }}
+
+                @csrf
+
+                <div class="field">
+
+                    <label for="title" class="label">Title</label>
+
+                    <div class="control">
+                        <input type="text" class="input" name="title" placeholder="Title" value="{{ $project->title }}">
+                    </div>
+
+                </div>
+
+                <div class="field">
+
+                    <label for="description" class="label">Description</label>
+
+                    <div class="control">
+                        <textarea name="description" class="textarea" value="{{ $project->description }}"></textarea>
+                    </div>
+
+                </div>
+
+                <div class="field">
+
+                    <div class="control">
+
+                        <button type="submit" class="is-link button">Update Project</button>
+
+                    </div>
+
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+</div>
+
+
+@endsection
