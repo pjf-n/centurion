f794bfd6e4d54136a9ef1b19adea24d325e20093
Patrick Neary <patrick@red-fern.co.uk>
2019-09-26 16:28:41 +0100

    Wrapped create markup in a bootstrap container

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -4,28 +4,34 @@
 
 @section('Content')
 
-    <h1>Projects an that</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Projects an that</h1>
 
-    <form method="POST" action="/projects">
+                <form method="POST" action="/projects">
 
-        {{ csrf_field() }}
+                    {{ csrf_field() }}
 
-        <div>
+                    <div>
 
-            <input type="text" name="title" placeholder="Project Title">
+                        <input type="text" name="title" placeholder="Project Title">
 
+                    </div>
+
+                    <div>
+                        <input type="text" name="description" placeholder="Project Description">
+                    </div>
+
+                    <div>
+                        <button type="submit">Create Project</button>
+                    </div>
+
+
+                </form>
+            </div>
         </div>
-
-        <div>
-            <input type="text" name="description" placeholder="Project Description">
-        </div>
-
-        <div>
-            <button type="submit">Create Project</button>
-        </div>
-
-
-    </form>
+    </div>
 
 
 @endsection
