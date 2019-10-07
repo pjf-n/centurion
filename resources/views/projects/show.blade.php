90a61c0d9729f307c6234a8cbbd55499742cb4b2
Patrick Neary <pattyjn@hotmail.co.uk>
2019-10-07 18:38:34 +0100

    Removed update and delete options from the show projects view

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,11 +6,7 @@
     <div class="row">
         <div class="col">
             <h2>{{  $project->title  }} Show Page</h2>
-            <form action="/projects/{{ $project->id }}" method="POST">
 
-                {{ method_field('PATCH') }}
-
-                @csrf
 
                 <div class="field">
 
@@ -27,22 +23,13 @@
                     <label for="description" class="label">Description</label>
 
                     <div class="control">
-                        <textarea name="description" class="textarea" value="{{ $project->description }}"></textarea>
+                        <textarea name="description" class="textarea">{{ $project->description }}</textarea>
                     </div>
 
                 </div>
 
-                <div class="field">
 
-                    <div class="control">
-
-                        <button type="submit" class="is-link button">Update Project</button>
-
-                    </div>
-
-                </div>
-
-            </form>
+            <a href="/projects/{{ $project->id  }}/edit" class="is-link button">Edit Project</a>
 
         </div>
     </div>
