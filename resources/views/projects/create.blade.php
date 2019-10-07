a59db103302b2385cedc9facd1350736231b787c
Patrick Neary <pattyjn@hotmail.co.uk>
2019-10-07 18:38:04 +0100

    Added validation error handling to the form

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -7,28 +7,62 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Projects an that</h1>
+                <h1 class="my-5">Create a project</h1>
 
                 <form method="POST" action="/projects">
 
-                    {{ csrf_field() }}
+                    @csrf
 
-                    <div>
+                    <div class="field">
 
-                        <input type="text" name="title" placeholder="Project Title">
+                        <label for="title" class="label">Title</label>
+
+                        <div class="control">
+                            <input type="text" class="input {{ $errors->has('title') ? 'is-danger' : '' }}" name="title" placeholder="Title" value="{{ old('title') }}" >
+                        </div>
 
                     </div>
 
-                    <div>
-                        <input type="text" name="description" placeholder="Project Description">
+                    <div class="field">
+
+                        <label for="description" class="label">Description</label>
+
+                        <div class="control">
+                            <textarea name="description" class="textarea {{ $errors->has('description') ? 'is-danger' : '' }}" placeholder="Description">{{ old('description') }}</textarea>
+                        </div>
+
                     </div>
 
-                    <div>
-                        <button type="submit">Create Project</button>
-                    </div>
+                    <div class="field">
 
+                        <div class="control">
+
+                            <button type="submit" class="is-link button">Create Project</button>
+
+                        </div>
+
+                    </div>
 
                 </form>
+
+                @if( $errors->any())
+
+                    <div class="notification is-danger">
+
+                        <ul>
+
+                        @foreach( $errors->all() as $error )
+
+                            <li>{{ $error }}</li>
+
+                        @endforeach
+
+                        </ul>
+
+                    </div>
+
+                @endif
+
             </div>
         </div>
     </div>
