e29b22bcd3c3aa134d87842f9d45c2339d6dbeb1
Patrick Neary <pattyjn@hotmail.co.uk>
2019-11-10 19:02:41 +0000

    added projects tasks controller

---
+++ app/Http/Controllers/ProjectsTasksController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class ProjectsTasksController extends Controller
+{
+    //
+
+    public function update( Task $task)
+    {
+
+        $task->update([
+            'completed' => request()->has('completed')
+        ]);
+
+        return back();
+
+    }
+}
