4fbd42fa9c8d0af1b55cb06c93902cfcb3a53268
Patrick Neary <pattyjn@hotmail.co.uk>
2019-11-10 19:02:21 +0000

    Added the form to complete tasks on the project page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -29,6 +29,41 @@
                 </div>
 
 
+            @if ($project->tasks->count())
+
+            <h2 class="text-center">Tasks</h2>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col d-flex justify-content-center">
+                        @foreach($project->tasks as $task)
+
+
+                            <form action="/tasks/{{ $task->id }}" method="post">
+
+                                @method('PATCH')
+                                @csrf
+
+                                <label for="completed" class="checkbox">
+
+                                    <input type="checkbox" name="completed" onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }} >
+                                        {{ $task->description }}
+
+                                </label>
+
+
+                            </form>
+
+
+
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
+            @endif
+
+
             <a href="/projects/{{ $project->id  }}/edit" class="is-link button">Edit Project</a>
 
         </div>
